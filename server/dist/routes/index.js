3877797f83dfccfc66ca50a340fc1a450bb4d188
Lionel Messi <juangirelli@gmail.com>
2025-01-16 22:08:32 -0500

    seed

--- server/dist/routes/index.js
+++ server/dist/routes/index.js
@@ -3,7 +3,7 @@ import authRoutes from './auth-routes.js';
 import apiRoutes from './api/index.js';
 import { authenticateToken } from '../middleware/auth.js';
 const router = Router();
-router.use('/auth', authRoutes);
+router.use('/auth', authRoutes); // POST,GET /auth/login
 // TODO: Add authentication to the API routes
-router.use('/api', authenticateToken, apiRoutes);
+router.use('/api', authenticateToken, apiRoutes); // /api/...
 export default router;
